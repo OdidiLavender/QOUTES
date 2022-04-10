613c6b5bb2c412870f454ea7ee928bbf9fc440d8
OdidiLavender <odidilavender@gmail.com>
2022-04-10 23:31:11 +0300

    contains the class functions of the app

---
+++ src/app/quote.ts
@@ -0,0 +1,5 @@
+export class Quote {
+    constructor(public name:string,public author:string, public description:string,public likes:number,public dislikes:number, public datePosted:Date){
+        
+    }
+}
