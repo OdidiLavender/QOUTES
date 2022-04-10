5d0136a8691c6a309c4c7931c2010f1681a98d99
OdidiLavender <odidilavender@gmail.com>
2022-04-10 23:32:30 +0300

    contains the details of the form

---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit ,Output,EventEmitter} from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+
+  
+  @Input() quote!:Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  incrementLikes(){
+    let like = this.quote?.likes;
+    like++;
+    this.quote.likes = like;
+  }
+
+  incrementDislikes(){
+    let dislike = this.quote.dislikes;
+    dislike++;
+    this.quote.dislikes = dislike;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
